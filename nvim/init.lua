f705ae51870eb9e383faf58663c4ed9376b93bb0
Humanoid-Human <Humanoid-Human@users.noreply.github.com>
2026-03-23 10:40:30 -0400

    a

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,13 @@
 require('config.lazy')
 
-vim.cmd.colorscheme('catppuccin')
-
 vim.opt.tabstop    = 4
 vim.opt.shiftwidth = 4
 
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.rnu    = true
 
 vim.opt.clipboard = 'unnamedplus'
-
-vim.opt.syntax = 'on'
+vim.opt.syntax    = 'on'
 
 vim.keymap.set('n', '<C-BS>', '"_bdw')
 vim.keymap.set('i', '<C-BS>', '<C-W>')
@@ -20,4 +17,7 @@ vim.keymap.set('i', '<C-Del>', '<Esc>ce')
 
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
+vim.api.nvim_set_var('terminal_emulator', 'kitty')
+
+vim.cmd.colorscheme('catppuccin')
 vim.api.nvim_create_user_command('Tr', 'Neotree', {})
